ad745d124549d8afb02515e41de938d90b263bd1
melesedadi <dadimeles@yahoo.com>
2019-09-05 12:55:20 -0400

    2nd commit

--- src/Grade.java
+++ src/Grade.java
@@ -8,7 +8,9 @@ public class Grade {
         Scanner key = new Scanner(System.in);
         System.out.println(" Please enter student's score: ");
         score = key.nextInt();
-        if (score >= 97) {
+        if (score > 100) {
+            grade = "Error: score out of range ; enter the right score.";}
+        else if (score >= 97) {
             grade = "A+";
         } else if (score >= 94) {
             grade = "A";
@@ -29,8 +31,8 @@ public class Grade {
         } else if (score >= 60) {
             grade = "D";
         } else if (score < 60) {
-            grade = "Go back to school if you get below 60";
-        } else {
+            grade = "Go back to school if you get below 60";}
+     else {
             System.out.println(" Error: Enter the right score: ");
         }
         {
@@ -41,13 +43,45 @@ public class Grade {
             System.out.println("Do you want to enter another score (Y/N)?");
                   }
         grd = key.next();
-        if (grd.equalsIgnoreCase("Y")) {
+        while (grd.equalsIgnoreCase("Y")) {
             System.out.println(" Please enter student's score: ");
-            score = key.nextInt();}
-
-         else {
-            System.out.println("Thank you");
-        }
+            score = key.nextInt();
+            if (score > 100) {
+                grade = "Error: score out of range ; enter the right score.";}
+            else if (score >= 97) {
+                grade = "A+";
+            } else if (score >= 94) {
+                grade = "A";
+            } else if (score >= 90) {
+                grade = "A-";
+            } else if (score >= 87) {
+                grade = "B+";
+            } else if (score >= 84) {
+                grade = "B";
+            } else if (score >= 80) {
+                grade = "B-";
+            } else if (score >= 77) {
+                grade = "C+";
+            } else if (score >= 74) {
+                grade = "C";
+            } else if (score >= 70) {
+                grade = "C-";
+            } else if (score >= 60) {
+                grade = "D";
+            } else if (score < 60) {
+                grade = "Go back to school if you get below 60";}
+            else {
+                System.out.println(" Error: Enter the right score: ");
+            }
+            {
+                System.out.println("Your score is: " + score);
+            }
+            {System.out.println("Your score is: " + grade);}
+            {
+                System.out.println("Your score is: " + score);
+            }
+            {System.out.println("Your score is: " + grade);}
+                }
     }
 }
 
